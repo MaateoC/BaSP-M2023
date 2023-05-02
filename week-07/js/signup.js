67b20d21aad8d41ed20487edf1887b151b6db5fb
Mateo Carciente <mateocarciente1@gmail.com>
2023-05-02 17:45:11 -0300

    BaSP-W07-2023: last corrections on signup and login html and css

--- week-07/js/signup.js
+++ week-07/js/signup.js
@@ -11,54 +11,79 @@ var passwordInput = document.getElementById("password");
 var formInput = document.querySelector("form");
 var repeatPasswordInput = document.getElementById("repeat-password");
 var submitButtonInput = document.getElementById("submit-button");
+var pSignUpName = document.getElementById("name-sign-up");
+var pSignUpSurname = document.getElementById("surname-sign-up");
+var pSignUpId = document.getElementById("id-sign-up");
+var pSignUpBirthdate = document.getElementById("birthdate-sign-up");
+var pSignUpPhone = document.getElementById("phone-sign-up");
+var pSignUpAddress = document.getElementById("address-sign-up");
+var pSignUpLocation = document.getElementById("location-sign-up");
+var pSignUpZipCode = document.getElementById("zipCode-sign-up");
+var pSignUpEmail = document.getElementById("email-sign-up");
+var pSignUpPassword = document.getElementById("password-sign-up");
+var pSignUpRepeatPassword = document.getElementById("repeat-password-sign-up");
 var errorMessage = [];
 
 function validateName(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[0] = "Name is required";
+    errorMessage.push("Name is required");
+    pSignUpName.textContent = "Name is required";
     nameInput.classList.remove("input-correct");
     nameInput.classList.add("input-error");
+    pSignUpName.style.display = "block";
     return false;
   } else if (e.target.value.length < 4) {
-    errorMessage[0] = "More than 3 letters required";
+    errorMessage.push("More than 3 letters required");
+    pSignUpName.textContent = "More than 3 letters required";
     nameInput.classList.remove("input-correct");
     nameInput.classList.add("input-error");
+    pSignUpName.style.display = "block";
   } else {
     nameInput.classList.remove("input-error");
     nameInput.classList.add("input-correct");
+    pSignUpName.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
 
 function validateSurname(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[1] = "Surname is required";
+    errorMessage.push("Surname is required");
+    pSignUpSurname.textContent = "Surname is required";
     surnameInput.classList.remove("input-correct");
     surnameInput.classList.add("input-error");
+    pSignUpSurname.style.display = "block";
     return false;
   } else if (e.target.value.length < 4) {
-    errorMessage[1] = "More than 3 letters required";
+    errorMessage.push("More than 3 letters required");
+    pSignUpSurname.textContent = "More than 3 letters required";
     surnameInput.classList.remove("input-correct");
     surnameInput.classList.add("input-error");
+    pSignUpSurname.style.display = "block";
   } else {
     surnameInput.classList.remove("input-error");
     surnameInput.classList.add("input-correct");
+    pSignUpSurname.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
 
 function validateId(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[2] = "ID required";
+    errorMessage.push("ID required");
+    pSignUpId.textContent = "ID required";
     idInput.classList.remove("input-correct");
     idInput.classList.add("input-error");
-    console.log(errorMessage[2]);
+    pSignUpId.style.display = "block";
     return false;
   }
   if (e.target.value.length < 7) {
-    errorMessage[2] = "More than 7 numbers required";
+    errorMessage.push("More than 7 numbers required");
+    pSignUpId.textContent = "More than 7 numbers required";
     idInput.classList.add("input-error");
-    console.log(errorMessage[2]);
+    pSignUpId.style.display = "block";
     return false;
   }
   var containLetters = false;
@@ -69,36 +94,48 @@ function validateId(e) {
     }
   }
   if (containLetters) {
-    errorMessage[2] = "This is not a number";
+    errorMessage.push("This is not a number");
+    pSignUpId.textContent = "This is not a number";
     idInput.classList.add("input-error");
+    pSignUpId.style.display = "block";
     return false;
   }
   idInput.classList.remove("input-error");
   idInput.classList.add("input-correct");
+  pSignUpId.style.display = "none";
+  errorMessage.splice(0, errorMessage.length);
   return true;
 }
 
 function validateBirthdate(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[3] = "Date required";
+    errorMessage.push("Date required");
+    pSignUpBirthdate.textContent = "Date required";
     birthdateInput.classList.add("input-error");
+    pSignUpBirthdate.style.display = "block";
     return false;
   } else {
     birthdateInput.classList.remove("input-error");
     birthdateInput.classList.add("input-correct");
+    pSignUpBirthdate.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
   }
   return true;
 }
 
 function validatePhoneNumber(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[4] = "Phone Number required";
+    errorMessage.push("Phone Number required");
+    pSignUpPhone.textContent = "Phone Number required";
     phoneNumberInput.classList.add("input-error");
+    pSignUpPhone.style.display = "block";
     return false;
   }
   if (e.target.value.length < 10) {
-    errorMessage[4] = "It must contain 10 numbers";
+    errorMessage.push("It must contain 10 numbers");
+    pSignUpPhone.textContent = "It must contain 10 numbers";
     phoneNumberInput.classList.add("input-error");
+    pSignUpPhone.style.display = "block";
   }
   var containLetters = false;
   for (let i = 0; i < e.target.value.length; i++) {
@@ -107,12 +144,16 @@ function validatePhoneNumber(e) {
     }
   }
   if (containLetters) {
-    errorMessage[4] = "This is not a number";
+    errorMessage.push("This is not a number");
+    pSignUpPhone.textContent = "This is not a number";
     phoneNumberInput.classList.add("input-error");
+    pSignUpPhone.style.display = "block";
   }
   if (e.target.value.length == 10) {
     phoneNumberInput.classList.remove("input-error");
     phoneNumberInput.classList.add("input-correct");
+    pSignUpPhone.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
   }
   return true;
 }
@@ -123,12 +164,16 @@ function validateAdress(e) {
   var isNumber = true;
 
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[5] = "Adress required";
+    errorMessage.push("Adress required");
+    pSignUpAddress.textContent = "Adress required";
     adressInput.classList.add("input-error");
+    pSignUpAddress.style.display = "block";
     return false;
   } else if (e.target.value.length < 5) {
-    errorMessage[5] = "Adress must contain at least 5 characters";
+    errorMessage.push("Adress must contain at least 5 characters");
+    pSignUpAddress.textContent = "Adress must contain at least 5 characters";
     adressInput.classList.add("input-error");
+    pSignUpAddress.style.display = "block";
     return false;
   }
   if (isNaN(Number(e.target.value[indexOfSpace - 1]))) {
@@ -138,25 +183,33 @@ function validateAdress(e) {
     isNumber = false;
   }
   if (!isLetter || !isNumber || indexOfSpace == -1) {
-    errorMessage[5] = "Blank space must be between a letter and a number";
+    errorMessage.push("Space must be between a letter and a number");
+    pSignUpAddress.textContent = "Space must be between a letter and a number";
     adressInput.classList.add("input-error");
+    pSignUpAddress.style.display = "block";
     return false;
   } else {
     adressInput.classList.remove("input-error");
     adressInput.classList.add("input-correct");
+    pSignUpAddress.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
 
 function validateLocation(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[6] = "Location required";
+    errorMessage.push("Location required");
+    pSignUpLocation.textContent = "Location required";
     locationInput.classList.add("input-error");
+    pSignUpLocation.style.display = "block";
     return false;
   }
   if (e.target.value.length < 4) {
-    errorMessage[6] = "More than 3 characters required";
+    errorMessage.push("More than 3 characters required");
+    pSignUpLocation.textContent = "More than 3 characters required";
     locationInput.classList.add("input-error");
+    pSignUpLocation.style.display = "block";
     return false;
   }
   var containLetters = false;
@@ -166,26 +219,36 @@ function validateLocation(e) {
     }
   }
   if (!containLetters) {
-    errorMessage[6] = "Must contain at least one letter";
+    errorMessage.push("Must contain at least one letter");
+    pSignUpLocation.textContent = "Must contain at least one letter";
     locationInput.classList.add("input-error");
+    pSignUpLocation.style.display = "block";
     return false;
   } else {
     locationInput.classList.remove("input-error");
     locationInput.classList.add("input-correct");
+    pSignUpLocation.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
 
 function validateZipCode(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[7] = "Zip Code required";
+    errorMessage.push("Zip Code required");
+    pSignUpZipCode.textContent = "Zip Code required";
     zipCodeInput.classList.add("input-error");
+    pSignUpZipCode.style.display = "block";
     return false;
   }
   if (e.target.value.length < 4 || e.target.value.length > 5) {
-    errorMessage[7] =
+    errorMessage.push(
+      "It must contain more than 4 numbers and less than 5 numbers"
+    );
+    pSignUpZipCode.textContent =
       "It must contain more than 4 numbers and less than 5 numbers";
     zipCodeInput.classList.add("input-error");
+    pSignUpZipCode.style.display = "block";
     return false;
   }
   var containLetters = false;
@@ -195,13 +258,16 @@ function validateZipCode(e) {
     }
   }
   if (containLetters) {
-    errorMessage[7] = "It only have to contain numbers";
+    errorMessage.push("It only have to contain numbers");
+    pSignUpZipCode.textContent = "It only have to contain numbers";
     zipCodeInput.classList.add("input-error");
-    console.log(errorMessage[7]);
+    pSignUpZipCode.style.display = "block";
     return false;
   } else {
     zipCodeInput.classList.remove("input-error");
     zipCodeInput.classList.add("input-correct");
+    pSignUpZipCode.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
@@ -209,29 +275,40 @@ function validateZipCode(e) {
 function validateEmail(e) {
   var validateEmail = /^[^@]+@[^@]+.[a-zA-Z]{2,}$/;
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[8] = "Email is required";
+    errorMessage.push("Email is required");
+    pSignUpEmail.textContent = "Email is required";
     emailInput.classList.add("input-error");
+    pSignUpEmail.style.display = "block";
     return false;
   }
   if (!validateEmail.test(emailInput.value)) {
-    errorMessage[8] = "Invalid email";
+    errorMessage.push("Invalid email");
+    pSignUpEmail.textContent = "Invalid email";
     emailInput.classList.add("input-error");
+    pSignUpEmail.style.display = "block";
   } else {
     emailInput.classList.remove("input-error");
     emailInput.classList.add("input-correct");
+    pSignUpEmail.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
   }
   return true;
 }
 
 function validatePassword(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[9] = "Password is required";
+    errorMessage.push("Password is required");
+    pSignUpPassword.textContent = "Password is required";
     passwordInput.classList.add("input-error");
+    pSignUpPassword.style.display = "block";
     return false;
   }
   if (e.target.value.length < 8 || e.target.value.length > 40) {
-    errorMessage[9] = "Password must contain between 8 and 40 characters";
+    errorMessage.push("Password must contain between 8 and 40 characters");
+    pSignUpPassword.textContent =
+      "Password must contain between 8 and 40 characters";
     passwordInput.classList.add("input-error");
+    pSignUpPassword.style.display = "block";
     return false;
   }
   var containsNumber = false;
@@ -241,8 +318,10 @@ function validatePassword(e) {
     }
   }
   if (!containsNumber) {
-    errorMessage[9] = "Password must contain at least one number";
+    errorMessage.push("Password must contain at least one number");
+    pSignUpPassword.textContent = "Password must contain at least one number";
     passwordInput.classList.add("input-error");
+    pSignUpPassword.style.display = "block";
     return false;
   }
   var containLetters = false;
@@ -252,28 +331,38 @@ function validatePassword(e) {
     }
   }
   if (!containLetters) {
-    errorMessage[9] = "Password must contain at least one letter";
+    errorMessage.push("Password must contain at least one letter");
+    pSignUpPassword.textContent = "Password must contain at least one letter";
     passwordInput.classList.add("input-error");
+    pSignUpPassword.style.display = "block";
     return false;
   } else {
     passwordInput.classList.remove("input-error");
     passwordInput.classList.add("input-correct");
+    pSignUpPassword.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
 
 function validateRepeatPassword(e) {
   if (e.target.value == "" || e.target.value == null) {
-    errorMessage[10] = "Password is required";
+    errorMessage.push("Password is required");
+    pSignUpRepeatPassword.textContent = "Password is required";
     repeatPasswordInput.classList.add("input-error");
+    pSignUpRepeatPassword.style.display = "block";
     return false;
   } else if (passwordInput.value !== repeatPasswordInput.value) {
-    errorMessage[10] = "The passwords must match";
+    errorMessage.push("The passwords must match");
+    pSignUpRepeatPassword.textContent = "The passwords must match";
     repeatPasswordInput.classList.add("input-error");
+    pSignUpRepeatPassword.style.display = "block";
     return false;
   } else {
     repeatPasswordInput.classList.remove("input-error");
     repeatPasswordInput.classList.add("input-correct");
+    pSignUpRepeatPassword.style.display = "none";
+    errorMessage.splice(0, errorMessage.length);
     return true;
   }
 }
